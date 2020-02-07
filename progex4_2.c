3f7328ccbba3eeb6780c2d75f35fc4c0c8fdb6bf
Robert <raguil42@mywvm.wvm.edu>
2020-02-07 15:48:13 -0800

    adding program from week 2 assignments

---
+++ progex4_2.c
@@ -0,0 +1,23 @@
+/* HW2_1 from Prgrmng Ex. 2. in Chapter 4, February 07, 2020 
+This program requests your first name and does the following with it:
+a. Prints it enclosed in double quoatation marks
+b. Prints it in a field 20 characters wide, with the whole field in quotes 
+   and the name at the right end of the field 
+c. Prints it at the left end of a field 20 characters wide, with the whole 
+   field enclosed in quotes
+d. Prints it in a field three characters wider than the name */
+
+#include <stdio.h>
+int main(void)
+{
+
+ 
+    
+    printf("Please enter your first name: ");
+    scanf("%", &);
+    printf("");
+    printf("");
+    printf("");
+
+    return 0;
+}
